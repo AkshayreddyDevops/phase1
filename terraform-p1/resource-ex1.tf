c9cd2a202f4b58e64d8e1820ce7b826f2f515219
reddy.akshay.gaddam <reddy.akshay.gaddam@gmail.com>
2024-07-20 21:00:27 +0530

    resource-ex1

---
+++ terraform-p1/resource-ex1.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "ec2"{
+  ami = data.aws_ami.resource-ext1.image_id
+  instance_type = "t3.micro" 
+  vpc_security_group_ids = [ data.aws_security_groups.resource-ext1.ids ]
+  tags = {
+    name = "frontend"
+  }
+}
