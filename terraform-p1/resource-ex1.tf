fd2952d0c190fde2002ec8c956e40470b5289980
reddy.akshay.gaddam <reddy.akshay.gaddam@gmail.com>
2024-07-20 21:33:06 +0530

    resource-ex1

--- terraform-p1/resource-ex1.tf
+++ terraform-p1/resource-ex1.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2"{
-  ami = data.aws_ami.resource-ext1.image_id
+  ami = var.aim
   instance_type = "t3.micro" 
   vpc_security_group_ids = data.aws_security_groups.resource-ext1.ids
   tags = {
---
+++ terraform-p1/variables.tf
@@ -0,0 +1,3 @@
+variable "aim" {
+  default = data.aws_ami.resource-ext1.image_id
+}
