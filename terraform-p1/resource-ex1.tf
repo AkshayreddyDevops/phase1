17080231ab2055fcf77ca8985b6c22918dc191a9
reddy.akshay.gaddam <reddy.akshay.gaddam@gmail.com>
2024-07-20 22:22:42 +0530

    mm

--- terraform-p1/resource-ex1.tf
+++ terraform-p1/resource-ex1.tf
@@ -1,6 +1,5 @@
 resource "aws_instance" "ec2"{
-  # ami = data.aws_ami.resource-ext1.image_id
-  ami = var.var_ami
+  ami = data.aws_ami.resource-ext1.image_id
   instance_type = "t3.micro" 
   vpc_security_group_ids = data.aws_security_groups.resource-ext1.ids
   tags = {
