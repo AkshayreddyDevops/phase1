f83b373159d9555e9cd19343b7b4cae67c1742ef
reddy.akshay.gaddam <reddy.akshay.gaddam@gmail.com>
2024-07-21 16:38:43 +0530

    module

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "test"{
   ami = "ami-041e2ea9402c46c32"
   vpc_security_group_ids = ["sg-0690f9908ac885f47"]
+  instance_type = "t3.micro"
   tags = {
     Name = "test"
   }
