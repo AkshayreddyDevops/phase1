419437963b0fce550fd77eb95a2f29006421271e
reddy.akshay.gaddam <reddy.akshay.gaddam@gmail.com>
2024-07-21 13:34:55 +0530

    vars

--- terraform-p1/resource-ex1.tf
+++ terraform-p1/resource-ex1.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2"{
   for_each = var.instance_name
   ami = data.aws_ami.resource-ext1.image_id
   vpc_security_group_ids = data.aws_security_groups.resource-ext1.ids
-  #instance_type = each.key
+  instance_type = "t3.micro"
   tags = {
     Name = each.key
   }
